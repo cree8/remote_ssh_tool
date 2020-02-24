9ee3a6a2d69810ed366ab5405f0cc0f01a4d039e
cree8 <40502425+cree8@users.noreply.github.com>
2020-02-24 15:32:15 -0600

    Additional features
    Began work on making tool.py able to determine what commands to apply to the remote host based on the result of comparing the following values:
    - The entered string (this should be based on what is the expected remote host output after connecting)
    - The actual host output 
    This has not yet been tested, and is in pre-alpha stage.

--- tool.py
+++ tool.py
@@ -1,4 +1,7 @@
 from fabric import Connection
+from paramiko import SSHException, AuthenticationException, BadAuthenticationType
+import socket
+
 
 commands =['esxcli system snmp set -r #clear v2 community',
            'esxcli system snmp set -a SHA1 -x AES128',
@@ -10,6 +13,7 @@ commands =['esxcli system snmp set -r #clear v2 community',
 #perform the functions on the remote devices
 def snmp_function(hostname,password):
     con = Connection(host='root@'+hostname, connect_kwargs={'password':password}) #Construct connection
+    
     if process_input(con):
         print ("No change needed for server "+ hostname)
         write_log(hostname + " was not changed")
@@ -17,7 +21,7 @@ def snmp_function(hostname,password):
     else:
         write_data(con,hostname)
     
-
+#Apply commands 
 def write_data(c,host):
     for command in commands:
         c.run(command, hide=True)
@@ -25,32 +29,51 @@ def write_data(c,host):
         write_log(host + " was changed")
 
 
-
+#Determine if server should be processed
 def process_input(c):
-    snmp_data = c.run(commands[4], hide=True).stdout    #read SNMP data from server, store into a (long)string
-    snmp_data_list = [line.strip() for line in snmp_data.splitlines()] #work the string into a list
-    
-    if snmp_data_list[0] == 'Authentication: SHA1' and snmp_data_list[1] == 'Communities:':
-        return True
-    else:
-        return False
+    try:
+        snmp_data = c.run(commands[4], hide=True).stdout    #read SNMP data from server, store into a (long)string
+        snmp_data_list = [line.strip() for line in snmp_data.splitlines()] #work the string into a list
+        if snmp_data_list[0] == 'Authentication: SHA1' and snmp_data_list[1] == 'Communities:':
+            return True
+        else:
+            return False
+        
+    except AuthenticationException: 
+        print ("Bad authentication credentials provided.")
+    except BadAuthenticationType: 
+        print ("Authentication type not allowed.")
+    except socket.gaierror:
+        print ("DNS resolution failed for " + str(c.host)) 
+        
+   
    
 
 def main():
+    host_list_placeholder = open(r"C:\Users\preed\Documents\Code Junk\FILES\VM_SERVER_LIST.txt",'r')
     target_host_passwd = input ("Enter the password: ")
-    #For each host in the list, run the function
-    host_list = [line.rstrip() for line in open(r"C:\Users\preed\Documents\Code Junk\VM_SERVER_LIST.txt",'r')]
+    
+    if (host_list_placeholder[0] != '' or host_list_placeholdeer):
+        host_list = [line.rstrip() for line in host_list_placeholder]
     for host in host_list:
         print ("Working on host: " + host)
         snmp_function(host, target_host_passwd)
         
 def write_log(string):
-    f = open(r"C:\Users\preed\Documents\Code Junk\SNMP_LOG.txt",'a+')
+    f = open(r"C:\Users\preed\Documents\Code Junk\FILES\SNMP_LOG.txt",'a+')
     f.write('\n' + string)
     f.close
 
+#this function is a way to cleanly open files with no errors, and create missing files only when flag is set
+def open_file(path_string, create_flag):
+    try:
+        print()
+    except AuthenticationException:
+        print()
+
+
 main()
 print ("Done!")
-
+exit()
 
 
